55f85f7b168d64b94a00b58acc07baf019464ae0
shashidhardemo92 <Shashidharreddy8@gmail.com>
2025-03-30 22:46:55 +0000

    converstionof Datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = "a33av"
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//note: "33" = 33 // converts to number
+// "33adv" = NaN
+// true = 1; false = 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// "" = false // its converts to false
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
